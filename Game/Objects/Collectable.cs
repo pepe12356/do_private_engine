9bdf1b2255ed99da7161856f911ea6455e96135c
pepe12356 <kulipeter74@gmail.com>
2025-12-31 22:18:46 +0100

    Rövid leírás

--- Game/Objects/Collectable.cs
+++ Game/Objects/Collectable.cs
@@ -104,6 +104,8 @@ namespace Ow.Game.Objects
 				p.SendPacket("0|A|STM|msg_booty-key-red_auto_buy_not_active");
 			else if (this is GreenBooty)
 				p.SendPacket("0|A|STM|msg_booty-key-green_auto_buy_not_active");
+			else if (this is GoldBooty)
+				p.SendPacket("0|A|STM|msg_booty-key-gold_auto_buy_not_active");
             }
             
             Character = null;
